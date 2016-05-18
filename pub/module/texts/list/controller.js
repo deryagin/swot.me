f4424d372e7f8554366b460cfe293c26834b7f2d
deryagin <deryagin.and@gmail.com>
2016-05-18 19:30:58 +0300

    Enh. Additional actions within text/list moved to drop-down menu.

--- pub/module/texts/list/controller.js
+++ pub/module/texts/list/controller.js
@@ -6,5 +6,36 @@ define([
   return function TextsListController() {
 
     TextsListController.$inject = [];
+
+    this.textList = [
+      {
+        id: 11111,
+        name: 'Criminal Minds - 1x02'
+      },
+      {
+        id: 22222,
+        name: 'Avatar (2009).TS.ViSiON.en'
+      },
+      {
+        id: 33333,
+        name: 'Criminal Minds - 1x01 - Extreme Aggressor.en'
+      },
+      {
+        id: 44444,
+        name: 'Criminal Minds - 1x01'
+      },
+      {
+        id: 55555,
+        name: 'The.Passionate.Programmer.May.2009'
+      },
+      {
+        id: 66666,
+        name: 'In Treatment - 1x02 - Alex - Week One.0TV.en'
+      },
+      {
+        id: 77777,
+        name: 'In Treatment - 1x01 - Laura - Week One.2HD.en'
+      }
+    ];
   };
 });
