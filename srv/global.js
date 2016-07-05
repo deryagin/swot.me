f260fb05c67d8aa738e8b5dd7c5aef7b6194bb54
deryagin <deryagin.and@gmail.com>
2016-07-05 21:29:28 +0300

    Enh. gns prefix added for global namespace vars.

--- srv/global.js
+++ srv/global.js
@@ -1,11 +1,6 @@
 var path = require('path');
 
-/**
- * global.swot - is a namespace designed to store all global vars of this project.
- *
- * @type {Object}
- */
-global.swot = {
+module.exports = {
 
   /** @type {String} - absolute path to the root directory of this project. */
   ROOTDIR: path.normalize(__dirname + '/../')
--- srv/server.js
+++ srv/server.js
@@ -1,10 +1,9 @@
-require('./global');
-
-var app = require(swot.ROOTDIR + 'srv/vendor/node_modules/express')();
+var gns = require('./global');
+var app = require(gns.ROOTDIR + 'srv/vendor/node_modules/express')();
 var server = require('http').createServer(app);
 
-var configureExpress = require(swot.ROOTDIR + 'srv/config/express');
-var configureApp = require(swot.ROOTDIR + 'srv/config/app');
+var configureExpress = require(gns.ROOTDIR + 'srv/config/express');
+var configureApp = require(gns.ROOTDIR + 'srv/config/app');
 
 configureExpress(app);
 configureApp(app);
