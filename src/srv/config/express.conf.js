56505cb05e692e71da725419345694277502166e
deryagin <deryagin.and@gmail.com>
2016-07-15 19:25:35 +0300

    New. [srv] ExpressJS body-parser added.

--- src/srv/config/express.conf.js
+++ src/srv/config/express.conf.js
@@ -1,7 +1,9 @@
 var express = require(swot.VENDOR_DIR + 'express');
+var bodyParser = require(swot.VENDOR_DIR + 'body-parser');
 
 module.exports.configure = configure;
 
 function configure(app) {
   app.use(express.static('src/pub/'));
+  app.use(bodyParser.json());
 }
