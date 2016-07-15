b71a54fa6f49fd3d5f5e984462c0c48303db8adf
deryagin <deryagin.and@gmail.com>
2016-07-15 19:50:09 +0300

    New. [srv] ExpressJS compression added.

--- src/srv/config/express.conf.js
+++ src/srv/config/express.conf.js
@@ -1,9 +1,11 @@
 var express = require(swot.VENDOR_DIR + 'express');
 var bodyParser = require(swot.VENDOR_DIR + 'body-parser');
+var compression = require(swot.VENDOR_DIR + 'compression');
 
 module.exports.configure = configure;
 
 function configure(app) {
+  app.use(compression());
   app.use(express.static('src/pub/'));
   app.use(bodyParser.json());
 }
