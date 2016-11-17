3844910705f22f0df46f73181484e9e537ee8d40
deryagin <deryaginav@yandex-team.ru>
2016-11-17 13:14:42 +0300

    New. [srv] Errors configure added.

--- src/srv/server.js
+++ src/srv/server.js
@@ -1,11 +1,12 @@
-require('./global');
-
-var app = require(swot.VENDOR_DIR + 'express')();
+global.swot = require('./globals');
+var app = require('express')();
 var server = require('http').createServer(app);
 var express = require('./config/express.conf');
 var routes = require('./config/routes.conf');
+var errors= require('./config/errors.conf');
 
 express.configure(app);
 routes.configure(app);
+errors.configure(app);
 server.listen(3000, '0.0.0.0');
 
