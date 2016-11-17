7dd4131d5e806e11aa61f57c84368f313f33b39c
deryagin <deryaginav@yandex-team.ru>
2016-11-17 13:13:48 +0300

    Enh. [srv] global.js renamed to globals.js.

---
+++ src/srv/globals.js
@@ -0,0 +1,9 @@
+/** @type {Object} - global.swot is a global vars namespace for this Back-End. */
+module.exports = {
+  
+  /** @type {String} - absolute path to this Back-End directory. */
+  SERVER_DIR: __dirname + '/',
+
+  /** @type {String} - absolute path to the vendor directory for this Back-End. */
+  VENDOR_DIR: __dirname + '/vendor/node_modules/'
+};
--- src/web/utility/rest.service.js
+++ src/web/utility/rest.service.js
@@ -8,24 +8,22 @@ define([
 
     var self = this;
 
-    self.baseUrl = baseUrl;
-
     self.transport = new HttpTransport();
 
     self.create = function create(json) {
-      return self.transport.send(self.baseUrl + '/create', json);
+      return self.transport.send(baseUrl + '/create', json);
     };
 
     self.read = function read(json) {
-      return self.transport.send(self.baseUrl + '/read', json);
+      return self.transport.send(baseUrl + '/read', json);
     };
 
     self.update = function update(json) {
-      return self.transport.send(self.baseUrl + '/update', json);
+      return self.transport.send(baseUrl + '/update', json);
     };
 
     self.delete = function deleteIt(json) {
-      return self.transport.send(self.baseUrl + '/delete', json);
+      return self.transport.send(baseUrl + '/delete', json);
     };
   }
 });
